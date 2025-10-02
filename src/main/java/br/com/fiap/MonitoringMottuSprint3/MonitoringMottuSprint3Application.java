d949bdcee32f8f6fdcd65469dd0c7c0d6254e5c5
Caepena <caepenafiel@gmail.com>
2025-10-01 22:50:58 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/MonitoringMottuSprint3/MonitoringMottuSprint3Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.MonitoringMottuSprint3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringMottuSprint3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringMottuSprint3Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/MonitoringMottuSprint3/MonitoringMottuSprint3ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.MonitoringMottuSprint3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringMottuSprint3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
